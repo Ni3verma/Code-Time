f436fa626984a053fdf67909c92624cf07703531
Nitin <canvas.nv@gmail.com>
2019-04-28 00:20:16 +0530

    contest detail UI

--- app/src/main/java/com/nitin/codetime/ui/contestDetail/ContestDetailFragment.kt
+++ app/src/main/java/com/nitin/codetime/ui/contestDetail/ContestDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.nitin.codetime.ui.contestDetail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +16,9 @@ import kotlinx.coroutines.launch
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.factory
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.FormatStyle
 
 class ContestDetailFragment : ScopedFragment(), KodeinAware {
 
@@ -52,7 +56,37 @@ class ContestDetailFragment : ScopedFragment(), KodeinAware {
         contestDetail.observe(this@ContestDetailFragment, Observer { contest ->
             if (contest == null) return@Observer
 
-            text_view.text = contest.toString()
+            Log.d("Nitin", contest.toString())
+            poster.setImageResource(getLogo(contest.resourceEntry.name))
+            name.text = contest.name
+            updateDates(contest.startDate, contest.endDate)
         })
+    }
+
+    private fun updateDates(startDateTime: String, endDateTime: String) {
+        val start = LocalDateTime
+            .parse(startDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+            .format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
+            .split(" ")
+
+        val end = LocalDateTime
+            .parse(endDateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+            .format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
+            .split(" ")
+
+        start_date.text = "${start[0]}\n${start[1]}"
+        end_date.text = "${end[0]}\n${end[1]}"
+    }
+
+    private fun getLogo(res: String): Int {
+        return when (res) {
+            "codechef.com" -> R.drawable.codechef_poster
+            "topcoder.com" -> R.drawable.topcoder_poster
+            "hackerrank.com" -> R.drawable.hackerrank_poster
+            "hackerearth.com" -> R.drawable.hackerearth_poster
+            "codeforces.com" -> R.drawable.codeforces_logo
+            // TODO: change with app icon
+            else -> R.drawable.ic_launcher_background
+        }
     }
 }
