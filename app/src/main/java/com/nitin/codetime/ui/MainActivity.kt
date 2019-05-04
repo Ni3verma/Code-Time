3ff7f3efaeee6de4a6595eaf0afba25ebe9de864
Nitin <canvas.nv@gmail.com>
2019-05-04 15:28:08 +0530

    nav drawer set up

--- app/src/main/java/com/nitin/codetime/ui/MainActivity.kt
+++ app/src/main/java/com/nitin/codetime/ui/MainActivity.kt
@@ -4,24 +4,32 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.nitin.codetime.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.present_contests, R.id.past_contests, R.id.future_contests
+        ).setDrawerLayout(drawer_layout).build()
+
         bottom_nav.setupWithNavController(navController)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        nav_view.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, null)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
