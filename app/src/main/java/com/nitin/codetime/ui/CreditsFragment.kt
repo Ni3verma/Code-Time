59be8e53b225088393eb6539ae61868c1454c84b
Nitin <canvas.nv@gmail.com>
2019-05-15 10:23:59 +0530

    added credit fragment
    added new fragment, added in nav graph also. UI is complete except for proper logo's

---
+++ app/src/main/java/com/nitin/codetime/ui/CreditsFragment.kt
@@ -0,0 +1,23 @@
+package com.nitin.codetime.ui
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.nitin.codetime.R
+
+class CreditsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        (activity as MainActivity).setBottomNavVisibility(View.GONE)
+        return inflater.inflate(R.layout.fragment_credits, container, false)
+    }
+
+
+}
