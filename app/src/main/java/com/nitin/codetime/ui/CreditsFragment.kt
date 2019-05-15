07c38d6bf8a6082cebf6b90874c08e15a0c3f5eb
Nitin <canvas.nv@gmail.com>
2019-05-15 23:51:02 +0530

    click listener added
    open url in browser when clicked on clist and hdodenhof row

--- app/src/main/java/com/nitin/codetime/ui/CreditsFragment.kt
+++ app/src/main/java/com/nitin/codetime/ui/CreditsFragment.kt
@@ -1,6 +1,8 @@
 package com.nitin.codetime.ui
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 import com.nitin.codetime.R
+import kotlinx.android.synthetic.main.fragment_credits.*
 
 class CreditsFragment : Fragment() {
 
@@ -17,6 +20,26 @@ class CreditsFragment : Fragment() {
     ): View? {
         (activity as MainActivity).setBottomNavVisibility(View.GONE)
         return inflater.inflate(R.layout.fragment_credits, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        clist.setOnClickListener {
+            val url = "https://clist.by/api/v1/doc"
+            openURL(url)
+        }
+
+        hdodenhof.setOnClickListener {
+            val url = "https://github.com/hdodenhof/CircleImageView"
+            openURL(url)
+        }
+    }
+
+    private fun openURL(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 
 
